3c6b3570f96c12a2b1be8a32db6bcee4fc8c6ba6
alik147 <sidorenko_oi_1017@1511.ru>
2020-05-02 01:07:34 +0300

    save

---
+++ second_lab/polynomial.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+struct polynomial
+{
+	
+	T a[];
+};
+class polys
+{
+private:
+	int size;
+
+public:
+	polys();
+	~polys();
+	
+};
